7ea3794ffa04a7b341105a015bf2a70f726c7aae
kong lingyu <975879575@qq.com>
2021-09-04 00:48:04 +0800

    create

---
+++ src/homepage.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './homepage.style.scss';
+
+const Homepage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className = 'title'>title</h1>
+                    <span className='subtitle'>shop now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className = 'title'>title</h1>
+                    <span className='subtitle'>shop now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className = 'title'>title</h1>
+                    <span className='subtitle'>shop now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className = 'title'>title</h1>
+                    <span className='subtitle'>shop now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className = 'title'>title</h1>
+                    <span className='subtitle'>shop now</span>
+                </div>
+            </div>
+
+        </div> 
+    </div>
+)
+
+export default Homepage;
