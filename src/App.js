39f22935fcdda76c59d94eb05e94207ca0865e55
kong lingyu <975879575@qq.com>
2021-09-13 22:18:10 +0800

    router HOc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Homepage from './pages/homepage/homepage.component';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
+import Homepage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>hatpage</h1>
+  </div>
+)
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
+    <div >
+      <Route exact path = '/' component={Homepage}  />
+      <Route path = '/hats' component={HatsPage}  />
     </div>
   );
 }
