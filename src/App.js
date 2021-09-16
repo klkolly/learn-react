55524ff2e68f6f34298738c610dba8c5c7cd4e41
kong lingyu <975879575@qq.com>
2021-09-16 22:34:25 +0800

    header

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
+import Header from './components/header/header.components';
 import Homepage from './pages/homepage/homepage.component';
-
 import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div >
-      <Route exact path = '/' component={Homepage}  />
+      <Header />
+      <Switch>
+      <Route exact path = '/' component={Homepage} />
       <Route path = '/shop' component={ShopPage}  />
+      </Switch>
     </div>
   );
 }
